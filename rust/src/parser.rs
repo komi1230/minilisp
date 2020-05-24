e5b988de61b31017aab1a60a0fbe69040c4edce7
Yusuke Kominami <yukke.konan@gmail.com>
2020-05-24 17:35:55 +0900

    refactoring: part1

--- rust/src/parser.rs
+++ rust/src/parser.rs
@@ -1,28 +1,67 @@
 use std::collections::VecDeque;
 
+#[derive(Debug,Clone)]
+pub enum Val {
+    List(VecDeque<Val>),
+    Number(f64),
+    Symbol(String),
+}
+
+
+
 pub fn tokenize(s: &str) -> VecDeque<String> {
     // Spread each tokens
-    let spreaded = s.replace("(", " ( ").replace(")", " ) ");
-
-    // Split string to each tokens
-    let tokens: VecDeque<String> = spreaded
-        .trim()
+    s.replace("(", " ( ")
+        .replace(")", " ) ")
         .split_whitespace()
         .map(|item| item.to_string())
         .collect();
-
-    tokens
 }
 
-pub fn print(tokens: &mut VecDeque<String>) -> String {
-    let mut s = "".to_string();
-    for i in 0..(tokens.len()-1) {
-        if tokens[i] == "(" || tokens[i+1] == ")" {
-            s = s + &tokens[i];
-        } else {
-            s = s + &tokens[i] + " ";
-        }
+
+pub fn parse(program: &str) -> Val {
+    let mut tokens = tokenize(program);
+    read_from_tokens(&mut tokens)
+}
+
+
+fn read_from_tokens(tokens: &mut Vec<String>) -> Val {
+    if tokens.len() == 0 {
+        panic!("unexpected EOF while reading");
     }
-    s = s + &tokens[tokens.len()-1];
-    s
+    let token = tokens.remove(0);
+    // println!("reading token: '{}'", token);
+    if "(".to_string() == token {
+        let mut list: Vec<Val> = Vec::new();
+        while tokens[0] != ")".to_string() {
+            list.push(read_from_tokens(tokens));
+        }
+        tokens.remove(0); // pop off ")"
+        Val::List(list)
+    } else if ")".to_string() == token {
+        panic!("unexpected ')' while reading");
+    } else {
+        atom(token)
+    }
 }
+
+
+fn atom(token: String) -> Val {
+    // TODO: separate int/float types?
+    let number = token.parse::<f64>();
+    match number {
+        Ok(x) => Val::Number(x),
+        Err(_) => Val::Symbol(token),
+    }
+}
+
+
+fn symbol_true() -> Val {
+    Val::Symbol("#t".to_string())
+}
+
+
+fn symbol_false() -> Val {
+    Val::Symbol("#f".to_string())
+}
+
